57f2eb594a70f7f29c0044310a5cbb3559870ad1
B200735 <Hello Al>
2021-11-10 14:28:13 +0000

    Use the Use the EMBOSS Cons to get the consensus sequence, and use 'pullseq' to extract the seqs after blast.

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 ################################################################
-# Before Start this Assignment, Things I need to check
+# Before Start this Assignment, what I have thought
 
 # 1. Check the inputs is correct (without special characers)
 # 2. Check the number of relevent protein before download it all (more than 3000 or equal to 0 will start over, less than 25 will give warning)
@@ -15,8 +15,10 @@
 # Import section
 import string
 import sys
-import os, subprocess, shutil
+import os
+import subprocess
 import re
+import shutil
 
 ################################################################
 #Function defination area.
@@ -66,24 +68,16 @@ def ask_the_aim_pro():
     pro = input_deco("Please tell me the protein name you are interested in: " )
     while test_special(pro) != True:
         pro = input_deco("You in put included special character which is not allowed. \nPlease tell me the protein name you are interested in: " )
-    a = input_deco("Do you know which taxonomic group it is?(y/n) " )
-    if a.lower() == 'y':
-        tax = input_deco("Pleas tell me the name of its taxonomic group: " )
-        while test_special(tax) != True:
-            tax = input_deco("You in put included special character which is not allowed. \nPlease tell me the taxonomic group name: " )
-        for character in tax:
-            if character not in string.ascii_letters :
-                print_deco("I don't think number should be in taxonomic group name, we will search without group name.")
-                tax = False
-    else:
-        print_deco("It's ok that you don't know the taxonomic group, we can continue searching.")
-        tax = False
+    tax = input_deco("Pleas tell me the name of its taxonomic group: " )
+    while test_special(tax) != True:
+        tax = input_deco("You in put included special character which is not allowed. \nPlease tell me the taxonomic group name: " )
+    for character in tax:
+        if character not in string.ascii_letters :
+            print_deco("I don't think number should be in taxonomic group name, we will search without group name.")
+            tax = False
     print_deco("Finding the uids that related to " + str(pro))
     # Use esearch then efetch to get the UID values
-    if tax != False:
-        return pro, tax
-    else:
-        return pro, "No taxonomic"
+    return pro, tax
     
 
 def mkdir(folder_name):
@@ -114,18 +108,15 @@ def pro_tax_count(pro,tax):
 # Count the number of relative proteins
 def count_relative_protein(pro, tax):
     print_deco('Counting the number of relative protein')
-    if tax == "No taxonomic":
-        count = pro_count(pro)
-    else :
-        count =  pro_tax_count(pro,tax)
-    while count >= 30000 :
-        pro = input_deco("WARNINGS! There are more than 30000 relative protein! Please check if you have enter the right protein name! \nPlease enter the right name of the protein family:\n")
+    count = pro_count(pro)
+    while count >= 1000 :
+        pro = input_deco("WARNINGS! There are more than 1000 relative protein! Please check if you have enter the right protein name! \nPlease enter the right name of the protein family:\n")
         ask_the_aim_pro()
     while count == 0 :
         pro = input_deco("WARNINGS! \nThere is no such "+str(pro)+" protein in the database, Please start over!\nPlease enter the right name of the protein family:\n")
         ask_the_aim_pro()
-    if count <= 25:
-        i = input_deco("WARNINGS! There are less than 25 relative protein! Please check if you have enter the right protein name! \nIf you want to start over please type 'y', if you want to continue with this data please type 'c'")
+    if count <= 5:
+        i = input_deco("WARNINGS! There are less than 5 relative protein! Please check if you have enter the right protein name! \nIf you want to start over please type 'y', if you want to continue with this data please type 'c'")
         if i.lower() != "c":
             print_deco("Sorry. I don't understand what you mean. Let's start over.")
             ask_the_aim_pro()
@@ -137,13 +128,13 @@ def test_number(n):
             int(n)
             break
         except :
-            n = input("I don't think you enter the correct number, please enter again. ")
+            n = input("I don't think you entered a number, please enter again. ")
     return int(n)
 
 def seq_num_check(num):
-    # If the number of sequences is more than 2500, we can't continue due to the huge data.
-    if num >= 2500:
-        print_deco("Opps! There are more than 2500 sequences which is too large for us to handle. \nPlease restart this program with other protein.")
+    # If the number of sequences is more than 1000, we can't continue due to the huge data.
+    if num >= 1000:
+        print_deco("Opps! There are more than 1000 sequences which is too large for us to handle. \nPlease restart this program with other protein.")
         exit()
     
 
@@ -151,25 +142,27 @@ def clustalo_align(pro, tax):
     print_deco("I'm currently aligning the sequences.")
     # Align all the protein sequences using clustalo
     # With '--force' we don't need to check whether the ouput file is exsited.
-    subprocess.call('clustalo -i {0}_{1}.fasta -o {0}_{1}_align.fasta -v --force'.format(pro,tax), shell =True)
+    subprocess.call('clustalo -i ' + str(pro) + '_' + str(tax) + '.fasta -o ' + str(pro) + '_' + str(tax) + '_align.fasta -v --force', shell =True)
 
 
-def plotcon(pro, tax):
+def plotcon(filename):
+    # Plot consercation of a sequence alignment.
     print_deco("Now, let's plot consercation of a sequence alignment.")
     file_check("plotcon.svg")
-    subprocess.call('plotcon -sequence ' + str(pro) + '_' + str(tax) + '_align.fasta -graph svg', shell=True)
-    #ask the user if he/she want to visualise the result.
+    subprocess.call('plotcon -sequence ' + str(filename) + ' -winsize 4 -graph svg', shell=True)
+    # Ask the user if he/she wants to see the result.
     ans = input_deco("Do you want to see the graph of the conservaton? (y/n)")
     if ans.lower() == 'y':
+        # Use firefox to open the graph in order to let the uesr see the graph.
 	    subprocess.call('firefox plotcon.svg&', shell=True)
-    print_deco("This graph is also saved, you could check it later if you want to.")
+    print_deco("This graph is saved, you could check it later if you want to.")
 
 
 def clustalo_percent_change(pro, tax):
-    #ask the user if he/she want to see more details about clustalo.
+    # Ask the user if he/she want to calculate the percetage changes by using infoalign.
     ans = input_deco("Do you want to calculate the percetage changes by using infoalign? (y/n) ")
     if ans.lower() == 'y':
-        #download name part and %change part, and make a dictionary after sort.
+        #download name part and percetage change part, and make a dictionary after sort.
         print_deco("I'm calculating now, just be patient.")
         file_check('output1.infoalign')
         file_check('output2.infoalign')
@@ -182,16 +175,34 @@ def clustalo_percent_change(pro, tax):
         values = list(map(float, list2))
         dic = dict(zip(keys, values))
         dic = sorted(dic.items(), key=lambda values:values[1])
-        print_deco("The percentage changes are below and they are sorted from low to hign")
+        ans = input_deco("The percentage changes are sorted from low to hign.\nDo you want to store them? (y/n) ")
         print(dic)
+        if ans == 'y':
+            # Store the sequeces to the file called acc_bitscore_sorted.txt
+            file_check("clustalo_percent_change.txt")
+            file = open("clustalo_percent_change.txt", "w")
+            for i in dic:
+                file.write("\n" + str(i)) 
+            file.close()
+
+
+def create_blastdb(pro, tax):
+    # Make a blast database from fasta files and save them to an output for user when doing blastp processing. 
+    print_deco('I am creating a blastdb from fasta files')
+    file_check(str(pro) + "_" + str(tax) + "_database")
+    subprocess.call("makeblastdb -in " + str(pro) + "_" + str(tax) + ".fasta -dbtype prot -out " + str(pro) + "_" + str(tax) + "_database", shell = True)
+    print_deco('I have now created blastdb.')
+
+
+
 
 
 
 
 
 ################################################################
-# Actually run the program
-### Step 1. Find out want the user want, and download the data
+# Actually running part
+### Part 1. Find out want the user want, and download the data
 # Ask what the user want and check if the input is correct.
 pro, tax = ask_the_aim_pro()
 count_relative_protein(pro, tax)
@@ -206,7 +217,7 @@ file_check(str(pro) + "_" + str(tax) + ".txt")
 subprocess.call('esearch -db protein -query "{0}[prot]" -organism "{1}" | efetch -format docsum | xtract -pattern DocumentSummary -element Organism > {0}_{1}.txt'.format(pro,tax), shell=True)
 
 # Count the number of sequences and species.
-seq_num = len(open('{0}_{1}.txt'.format(pro,tax)).readlines())
+seq_num = len(open(str(pro) + '_' + str(tax) + '.txt').readlines())
 species_num = len(set(open('{0}_{1}.txt'.format(pro,tax)).readlines()))
 # Check if the number of sequences is too large.
 seq_num_check(seq_num)
@@ -218,14 +229,75 @@ if ans == 'q':
     exit()
 
 
-### Step 2. Analyze the conservation between the sequences we found.
+
+### Part 2. Analyze the conservation between the sequences we found.
 # Download the fasta files to the current directory.
+print_deco("Sure! Now I will download the fasta files that related to what you searching for.")
 subprocess.call('esearch -db protein -query "{0}[prot]" -organism "{1}" | efetch -format fasta > {0}_{1}.fasta'.format(pro,tax), shell=True)
 
 # Using clustalo to align the consevation area beatween all the sequences.
 clustalo_align(pro, tax)
-plotcon(pro, tax)
+
+# Plot consercation of a sequence alignment.
+plotcon(str(pro) + '_' + str(tax) + '_align.fasta')
+
+# Calculate the percentage changes.
 clustalo_percent_change(pro, tax)
+
+# Starting to do blastdb
+create_blastdb(pro, tax)
+
+# Using the EMBOSS Cons to get the consensus sequence from Clustalo output.
+print_deco('Using the EMBOSS Cons to get the consensus sequence from Clustalo output.')
+file_check(str(pro) + '_' + str(tax) + '_consensus.fasta')
+subprocess.call('cons -plurality 0.8 -sequence ' + str(pro) + '_' + str(tax) + '_align.fasta -outseq ' + str(pro) + '_' + str(tax) + '_consensus.fasta', shell=True) 
+print_deco('Consensus has been created now.')
+
+# Using blastp to blast the protein that the user is interested in.
+print_deco('Using blastp to search this file: ' + str(pro) + '_' + str(tax) + '_consensus.fasta in blastdb')
+subprocess.call('blastp -db ' + str(pro) + '_' + str(tax) + '_database -query ' + str(pro) + '_' + str(tax) + '_consensus.fasta -outfmt 7 -out ' + str(pro) + '_' + str(tax) + '_blast.txt', shell = True)
+
+# Creat a dictionary that contains the acc and the bit score.
+blastp_result = {}
+line = []
+for i in open(str(pro) + '_' + str(tax) + '_blast.txt', 'r'):
+    if '#' not in i:
+        line.append(i)
+print(line)
+with open(str(pro) + '_' + str(tax) + '_blast_lines.txt', 'w', encoding="utf-8") as my_file:
+    my_file.writelines(line)
+for i in line :
+    piece = i.split('\t')
+    blastp_result[piece[1]] = piece[-1]
+
+# Ask the user to enter a number of most similar sequence.
+ans = input_deco("I have sorted the related sequences by their bit score.\nHow many sequence you want to store? \nPlease enter an number.")
+while True :
+    ans = test_number(ans)
+    if ans <= 0:
+        ans = input_deco("The number is negtive. \nPlease enter again.")
+    else :
+        break
+print_deco("Now I'm storing the sequence for you.")
+
+# Store the sequeces to the file called acc_bitscore_sorted.txt
+file_check("acc_bitscore_sorted.txt")
+file = open("acc_bitscore_sorted.txt", "w")
+result = {}
+for i in sorted(blastp_result.items(),key=lambda item:item[1],reverse=True)[:ans]:
+    result[i[0]] = i[1]
+file.write("\n".join(result.keys())) 
+file.close()
+
+# Use the provided 'pullseq' programme to extract the sequences from the fasta file.
+file_check("acc_bitscore_sorted.fasta")
+print_deco("Now I'm ecxtracting sequencs from a fasta file")
+subprocess.call('/localdisk/data/BPSM/Assignment2/pullseq -i ' + str(pro) + '_' + str(tax) + '_align.fasta -n subject_acc.txt > acc_bitscore_sorted.fasta -v', shell=True)
+
+# Use plotcon to plot the similar sequences alignment result 
+ans = input_deco("Do you want to see the plot of blast result by plotcon? (y/n)")
+if ans.lower() == 'y':
+    plotcon("acc_bitscore_sorted.fasta")
 
 
 
